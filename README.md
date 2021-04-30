ec48b345433a3df4f106533ba6178581cd3021be
Wilian Webber Rech <wilianwrech@hotmail.com>
2021-04-30 16:21:46 -0300

    Startup

---
+++ README.md
@@ -0,0 +1,5 @@
+## Node JS REST API
+
+Basic structure of a Node REST API
+
+[Reference](https://www.toptal.com/express-js/nodejs-typescript-rest-api-pt-1).
---
+++ app.ts
@@ -0,0 +1,62 @@
+
+import express from 'express';
+import * as http from 'http';
+import * as winston from 'winston';
+import * as expressWinston from 'express-winston';
+import cors from 'cors';
+import { CommonRoutesConfig } from './common/common.routes.config';
+import { UsersRoutes } from './users/users.routes.config';
+import debug from 'debug';
+
+const app: express.Application = express();
+const server: http.Server = http.createServer(app);
+const port = 3000;
+const routes: Array<CommonRoutesConfig> = [];
+const debugLog: debug.IDebugger = debug('app');
+
+// here we are adding middleware to parse all incoming requests as JSON 
+app.use(express.json());
+
+// here we are adding middleware to allow cross-origin requests
+app.use(cors());
+
+// here we are preparing the expressWinston logging middleware configuration,
+// which will automatically log all HTTP requests handled by Express.js
+const loggerOptions: expressWinston.LoggerOptions = {
+    transports: [new winston.transports.Console()],
+    format: winston.format.combine(
+        winston.format.json(),
+        winston.format.prettyPrint(),
+        winston.format.colorize({ all: true })
+    ),
+};
+
+// here we are crashing on unhandled errors and spitting out a stack trace,
+// but only when in debug mode
+if (process.env.DEBUG) {
+    process.on('unhandledRejection', function(reason) {
+        debugLog('Unhandled Rejection:', reason);
+        process.exit(1);
+    });
+} else {
+    loggerOptions.meta = false; // when not debugging, make terse
+}
+
+// initialize the logger with the above configuration
+app.use(expressWinston.logger(loggerOptions));
+
+// here we are adding the UserRoutes to our array,
+// after sending the Express.js application object to have the routes added to our app!
+routes.push(new UsersRoutes(app));
+
+// this is a simple route to make sure everything is working properly
+app.get('/', (req: express.Request, res: express.Response) => {
+    res.status(200).send(`Server up and running!`)
+});
+
+server.listen(port, () => {
+  debugLog(`Server running at http://localhost:${port}`);
+  routes.forEach((route: CommonRoutesConfig) => {
+      debugLog(`Routes configured for ${route.getName()}`);
+  });
+});
